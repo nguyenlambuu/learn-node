7e7c3def8ad1abea768afe6fa6e9a12ec47a80aa
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2019-08-10 07:20:19 +0700

    Filter query

--- 4-natour/controllers/tourController.js
+++ 4-natour/controllers/tourController.js
@@ -2,8 +2,36 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
 	try {
-		const tours = await Tour.find();
+		// BUILD A QUERY
+		// 1/ Filtering
+		const queryObject = { ...req.query };
+		const excludedFields = ['page', 'sort', 'limit', 'fields'];
+		excludedFields.forEach(element => delete queryObject[element]);
 
+		// 	2/ Advanced Filtering
+
+		let queryString = JSON.stringify(queryObject);
+		queryString = queryString.replace(
+			/\b(gt|gte|lt|lte)\b/g,
+			match => `$${match}`
+		);
+		console.log(JSON.parse(queryString));
+
+		// { difficulty: 'easy', duration: { $gte: 4 }}
+		// { difficulty: 'easy', duration: { gte: '4' }}
+		// gt, gte, lt, lte
+
+		const query = await Tour.find(JSON.parse(queryString));
+
+		// EXECUTE QUERY
+		const tours = query;
+		// const query = await Tour.find()
+		// 	.where('duration')
+		// 	.gte(4)
+		// 	.where('difficulty')
+		// 	.equals('easy');
+
+		// SEND DATA
 		res.status(200).json({
 			status: 'success',
 			results: tours.length,
