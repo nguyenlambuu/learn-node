8e8151ab0193dee50c3918090e9baa5fb0f1c1a7
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2019-08-10 07:16:25 +0700

    Add properties for tourModel

--- 4-natour/models/tourModel.js
+++ 4-natour/models/tourModel.js
@@ -6,14 +6,50 @@ const tourSchema = new mongoose.Schema({
 		required: [true, 'A tour must have a name'],
 		unique: true
 	},
+	duration: {
+		type: Number,
+		required: [true]
+	},
+	maxGroupSize: {
+		type: Number,
+		required: [true, 'A tour must have a group size']
+	},
+	difficulty: {
+		type: String,
+		required: [true, 'A tour must have a difficult']
+	},
+	ratingsAverage: {
+		type: Number,
+		default: 4.5
+	},
+	ratingsQuantity: {
+		type: Number,
+		default: 0
+	},
 	price: {
 		type: Number,
 		required: [true, 'A tour must have a price']
 	},
-	rating: {
-		type: Number,
-		default: 4.5
-	}
+	priceDiscount: Number,
+	summary: {
+		type: String,
+		trim: true,
+		required: [true, 'A tour must have a description']
+	},
+	description: {
+		type: String,
+		trim: true
+	},
+	imageCover: {
+		type: String,
+		required: [true, 'A tour must have a cover image']
+	},
+	images: [String],
+	createdAt: {
+		type: Date,
+		default: Date.now()
+	},
+	startDates: [Date]
 });
 const Tour = mongoose.model('Tour', tourSchema);
 
