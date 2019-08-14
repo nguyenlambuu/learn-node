a799d0422e5ccc10fc22e0bfeb0cd6974572ee6b
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2019-08-14 15:37:16 +0700

    Build in anf custom validator

--- 4-natour/models/tourModel.js
+++ 4-natour/models/tourModel.js
@@ -1,12 +1,18 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+// const validator = require('validator');
 
 const tourSchema = new mongoose.Schema(
 	{
 		name: {
 			type: String,
 			required: [true, 'A tour must have a name'],
-			unique: true
+			unique: true,
+			trim: true,
+			maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+			minlength: [10, 'A tour name must have more or equal than 10 characters']
+			// Custom validate with validator.js
+			// validate: [validator.isAlpha, 'Tour name must only contains characters']
 		},
 		slug: String,
 		duration: {
@@ -19,11 +25,17 @@ const tourSchema = new mongoose.Schema(
 		},
 		difficulty: {
 			type: String,
-			required: [true, 'A tour must have a difficult']
+			required: [true, 'A tour must have a difficult'],
+			enum: {
+				values: ['easy', 'medium', 'difficult'],
+				message: 'Difficulty is either: easy, medium or difficult'
+			}
 		},
 		ratingsAverage: {
 			type: Number,
-			default: 4.5
+			default: 4.5,
+			min: [1, 'Rating must be above 1.0'],
+			max: [5, 'Rating must be below 5.0']
 		},
 		ratingsQuantity: {
 			type: Number,
@@ -33,7 +45,16 @@ const tourSchema = new mongoose.Schema(
 			type: Number,
 			required: [true, 'A tour must have a price']
 		},
-		priceDiscount: Number,
+		priceDiscount: {
+			type: Number,
+			validate: {
+				validator: function(val) {
+					// 'this' only point to current document on NEW document creation
+					return this.price >= val; // 200 > 100: true, 100 > 200: false;
+				},
+				message: `Discount price ({VALUE}) should be below regular price`
+			}
+		},
 		summary: {
 			type: String,
 			trim: true,
