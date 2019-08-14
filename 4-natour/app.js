b99500320b1edc9ccc4f796e527c57710c6d6056
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2019-08-14 14:08:57 +0700

    Remove example middleware

--- 4-natour/app.js
+++ 4-natour/app.js
@@ -13,11 +13,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-	console.log('Hello from the middleware 👋🏻 ');
-	next();
-});
-
 // 3. ROUTES
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
