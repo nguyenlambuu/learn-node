96cd50048c18d7f4d7f6e0c0b69d0d00ad9d68f3
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2019-08-14 13:29:02 +0700

    Virtual properties

--- 4-natour/models/tourModel.js
+++ 4-natour/models/tourModel.js
@@ -1,56 +1,68 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, 'A tour must have a name'],
-		unique: true
+const tourSchema = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+			required: [true, 'A tour must have a name'],
+			unique: true
+		},
+		duration: {
+			type: Number,
+			required: [true]
+		},
+		maxGroupSize: {
+			type: Number,
+			required: [true, 'A tour must have a group size']
+		},
+		difficulty: {
+			type: String,
+			required: [true, 'A tour must have a difficult']
+		},
+		ratingsAverage: {
+			type: Number,
+			default: 4.5
+		},
+		ratingsQuantity: {
+			type: Number,
+			default: 0
+		},
+		price: {
+			type: Number,
+			required: [true, 'A tour must have a price']
+		},
+		priceDiscount: Number,
+		summary: {
+			type: String,
+			trim: true,
+			required: [true, 'A tour must have a description']
+		},
+		description: {
+			type: String,
+			trim: true
+		},
+		imageCover: {
+			type: String,
+			required: [true, 'A tour must have a cover image']
+		},
+		images: [String],
+		createdAt: {
+			type: Date,
+			default: Date.now()
+		},
+		startDates: [Date]
 	},
-	duration: {
-		type: Number,
-		required: [true]
-	},
-	maxGroupSize: {
-		type: Number,
-		required: [true, 'A tour must have a group size']
-	},
-	difficulty: {
-		type: String,
-		required: [true, 'A tour must have a difficult']
-	},
-	ratingsAverage: {
-		type: Number,
-		default: 4.5
-	},
-	ratingsQuantity: {
-		type: Number,
-		default: 0
-	},
-	price: {
-		type: Number,
-		required: [true, 'A tour must have a price']
-	},
-	priceDiscount: Number,
-	summary: {
-		type: String,
-		trim: true,
-		required: [true, 'A tour must have a description']
-	},
-	description: {
-		type: String,
-		trim: true
-	},
-	imageCover: {
-		type: String,
-		required: [true, 'A tour must have a cover image']
-	},
-	images: [String],
-	createdAt: {
-		type: Date,
-		default: Date.now()
-	},
-	startDates: [Date]
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true }
+	}
+);
+
+// Virtual property is not really save into database, but user can query it
+tourSchema.virtual('durationWeeks').get(function() {
+	return this.duration / 7;
 });
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
