43339d0bc52a63ebe76362694b0847ec26ed02ac
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2019-10-17 20:54:17 +0700

    Siging up user

--- 4-natour/controllers/authController.js
+++ 4-natour/controllers/authController.js
@@ -1,17 +1,23 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
-	const userData = { ...req.body };
-	const newUser = await User.create({
-		name: userData.name,
-		email: userData.email,
-		password: userData.password,
-		passwordConfirm: userData.passwordConfirm
+	const { name, email, password, passwordConfirm } = req.body;
+	const user = await User.create({
+		name,
+		email,
+		password,
+		passwordConfirm
+	});
+
+	const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+		expiresIn: process.env.JWT_EXPIRES_IN
 	});
 
 	res.status(201).json({
 		status: 'success',
-		data: { user: newUser }
+		token,
+		data: { user }
 	});
 });
