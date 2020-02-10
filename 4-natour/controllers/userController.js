3ebe4142bfc0fa4a60962d07ae68e71b34fa272a
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2020-02-10 20:39:56 +0700

    Update user data: name and email

--- 4-natour/controllers/userController.js
+++ 4-natour/controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+const filterObj = (requestBody, ...allowedFields) => {
+	const newObjFiltered = {};
+	Object.keys(requestBody).forEach(el => {
+		if (allowedFields.includes(el)) newObjFiltered[el] = requestBody[el];
+	});
+	return newObjFiltered;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
 	const users = await User.find();
@@ -9,6 +18,30 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
 		results: users.length,
 		data: { users }
 	});
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+	// 1. Create error if user POSTs password data.
+	if (req.body.password || req.body.passwordConfirm) {
+		return next(
+			new AppError(
+				'This route is not for password updates. Please use /updateMyPassword',
+				400
+			)
+		);
+	}
+
+	// 2. Fillterd out unwanted fields names that are not allowed to be updated.
+	// We want to allow user to update there email, name, avatar...
+	const filteredBody = filterObj(req.body, 'name', 'email');
+
+	// 3.Update user document.
+	const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+		new: true,
+		runValidators: true
+	});
+
+	res.status(200).json({ status: 'success', data: { user: updatedUser } });
 });
 
 exports.getUser = (req, res) => {
