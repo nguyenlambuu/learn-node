7a5f3b830f36f4cc4fb0c630fd5e6476f293a230
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2020-02-11 07:40:16 +0700

    Setting security HTTP headers

--- 4-natour/app.js
+++ 4-natour/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const AppError = require('./utils/appError');
 const globalErrorController = require('./controllers/errorController');
@@ -9,21 +10,31 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1. MIDDLEWARES
+// 1. GLOBAL MIDDLEWARES
+
+// Set security HTTP headers.
+app.use(helmet());
+
+// Development logging.
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
+// Limit requests from same API.
 const limiter = rateLimit({
 	max: 100,
 	windowMs: 60 * 60 * 1000,
 	message: 'Too many request from an IP. Please try again in an hour!'
 });
-
 app.use('/api', limiter);
-app.use(express.json());
+
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+// Test middleware here.
 app.use((req, res, next) => {
 	req.requestTime = new Date().toISOString();
 	next();
