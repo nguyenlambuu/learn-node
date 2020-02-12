4ff021aa6864041b240ef2f285aa0901fcbbed0c
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2020-02-12 07:13:18 +0700

    Prevent parameters pollution

--- 4-natour/app.js
+++ 4-natour/app.js
@@ -4,6 +4,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorController = require('./controllers/errorController');
@@ -38,6 +39,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against XSS attack.
 app.use(xss());
+
+// Prevent parameters pollution, whitelist is allowed duplicate parameters.
+app.use(
+	hpp({
+		whitelist: [
+			'duration',
+			'ratingsQuantity',
+			'ratingsAverage',
+			'maxGroupSize',
+			'difficulty',
+			'price'
+		]
+	})
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
