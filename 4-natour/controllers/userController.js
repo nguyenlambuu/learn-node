958e0a5fb96dcc711c490dbcfc016d33820fab79
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2020-03-10 05:52:33 +0700

    Resizing user photo using Sharp

--- 4-natour/controllers/userController.js
+++ 4-natour/controllers/userController.js
@@ -1,19 +1,22 @@
 const multer = require('multer');
+const sharp = require('sharp');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 const factory = require('./handlerFactory');
 
-const multerStorage = multer.diskStorage({
-	destination: (req, file, cb) => {
-		cb(null, 'public/img/users');
-	},
-	filename: (req, file, cb) => {
-		// user-7167469294-12341235.jpeg
-		const extension = file.mimetype.split('/')[1];
-		cb(null, `user-${req.user.id}-${Date.now()}.${extension}`);
-	}
-});
+// const multerStorage = multer.diskStorage({
+// 	destination: (req, file, cb) => {
+// 		cb(null, 'public/img/users');
+// 	},
+// 	filename: (req, file, cb) => {
+// 		// user-7167469294-12341235.jpeg
+// 		const extension = file.mimetype.split('/')[1];
+// 		cb(null, `user-${req.user.id}-${Date.now()}.${extension}`);
+// 	}
+// });
+
+const multerStorage = multer.memoryStorage();
 
 const multerFilter = (req, file, cb) => {
 	if (file.mimetype.startsWith('image')) {
@@ -26,6 +29,19 @@ const multerFilter = (req, file, cb) => {
 const upload = multer({ storage: multerStorage, fileFilter: multerFilter });
 
 exports.uploadUserPhoto = upload.single('photo');
+
+exports.resizeUserPhoto = (req, res, next) => {
+	if (!req.file) return next();
+	req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+	sharp(req.file.buffer)
+		.resize(500, 500)
+		.toFormat('jpeg')
+		.jpeg({ quality: 90 })
+		.toFile(`public/img/users/${req.file.filename}`);
+
+	next();
+};
 
 const filterObj = (requestBody, ...allowedFields) => {
 	const newObjFiltered = {};
