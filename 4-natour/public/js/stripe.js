b66a5110350b137a3ab92a605ea927b16af2a8a1
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2020-03-17 06:44:19 +0700

    Processing payments on the front end

---
+++ 4-natour/public/js/stripe.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+
+const stripe = Stripe('pk_test_7Krn5clb9PQ75XdDzvVIfxMC0093epqJNv');
+
+export const bookTour = async tourId => {
+	try {
+		// 1. Get checkout sesssion from endpoint
+		const session = await axios(
+			`http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`
+		);
+		// 2. Create checkout form + chanre credit card
+		await stripe.redirectToCheckout({ sessionId: session.data.session.id });
+	} catch (err) {
+		showAlert('error', err);
+	}
+};
