dd0d2cdb7cc913ad1a5f1bc874a2b5f966315465
Buu Lam Nguyen <nguyenlambuu@gmail.com>
2020-03-18 05:34:17 +0700

    Bookings management

--- 4-natour/controllers/viewsController.js
+++ 4-natour/controllers/viewsController.js
@@ -1,4 +1,5 @@
 const Tour = require('../models/tourModel');
+const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
@@ -40,5 +41,18 @@ exports.getLoginForm = catchAsync(async (req, res) => {
 exports.getAccount = catchAsync(async (req, res) => {
 	res.status(200).render('account', {
 		title: 'Your account'
+	});
+});
+
+exports.getMyTours = catchAsync(async (req, res) => {
+	// 1. Find all bookings by user
+	const bookings = await Booking.find({ user: req.user.id });
+	// 2. Find tours with the returned IDs
+	const tourIDs = bookings.map(el => el.tour);
+	const tours = await Tour.find({ _id: { $in: tourIDs } });
+
+	res.status(200).render('overview', {
+		title: 'My Tours',
+		tours
 	});
 });
--- 4-natour/routes/viewRoutes.js
+++ 4-natour/routes/viewRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const viewsController = require('./../controllers/viewsController');
-const authController = require('./../controllers/authController');
+const viewsController = require('../controllers/viewsController');
+const authController = require('../controllers/authController');
+const bookingController = require('../controllers/bookingController');
 
 const router = express.Router();
 
-router.get('/', authController.isLoggedIn, viewsController.getOverview);
+router.get(
+	'/',
+	bookingController.createBookingCheckout,
+	authController.isLoggedIn,
+	viewsController.getOverview
+);
 router.get('/tours/:slug', authController.isLoggedIn, viewsController.getTour);
 router.get('/login', authController.isLoggedIn, viewsController.getLoginForm);
 router.get('/me', authController.protect, viewsController.getAccount);
+router.get('/my-tours', authController.protect, viewsController.getMyTours);
 module.exports = router;
